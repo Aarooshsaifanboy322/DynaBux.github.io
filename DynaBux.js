a66b2480e2de59b7055ebc8efa4c7ae561f6de7a
AarooshRBLX <164038746+Aarooshsaifanboy322@users.noreply.github.com>
2024-03-19 22:59:57 +0600

    Add files via upload

---
+++ DynaBux.js
@@ -0,0 +1,6 @@
+document.oncontextmenu = () => {
+    return false;
+}
+function Scam() {
+    window.alert("HA! GIT SCAMMED LOSER! AND YOUR ROBLOX GOT HACKED! NOW, WHEN YOU JOIN ANY GAME, IT WILL SAY 'Connection Error (ID = 17 Joining Attempt Failed)'")
+}
